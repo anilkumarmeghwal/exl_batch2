844183b75046eb71bcb8f65950e48808c65ed333
anilkumarmeghwal <101693397+anilkumarmeghwal@users.noreply.github.com>
2023-08-19 13:59:26 +0530

    Add files via upload

---
+++ PQ5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_questions
+{
+    internal class PQ5
+    {
+        static void Main(string[] args)
+        {
+           int a=10; int b=12;
+            int c;
+            c = a;
+            a = b;
+            b=c;
+            Console.WriteLine($"value of a is {a} and value of b is {b}");
+        }
+    }
+}
